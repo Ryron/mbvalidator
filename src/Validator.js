6c2b49cfe5201eb0992d4e8ea429e1abd690e4f5
ryron <ryronhung@163.com>
2017-08-31 23:20:16 +0800

    add 新增onkeyup、borderColor

--- src/Validator.js
+++ src/Validator.js
@@ -3,7 +3,6 @@ import _ from './utils';
 import Rules from './rules';
 let type = ['input:not([type]), input[type="color"], input[type="date"], input[type="datetime"], input[type="datetime-local"], input[type="email"], input[type="file"], input[type="hidden"], input[type="month"], input[type="number"], input[type="password"], input[type="range"], input[type="search"], input[type="tel"], input[type="text"], input[type="time"], input[type="url"], input[type="week"], textarea', 'select', 'input[type="checkbox"], input[type="radio"]'];
 let allTypes = type.join(',');
-
 class Validator {
 	constructor (form, settings, success, error) {
 			let self = this;
@@ -18,9 +17,9 @@ class Validator {
 				let $this = $(this);
 				if ($this.is(allTypes)) {
 					// 绑定onkeyup
-					// $this.on('keyup', function (event) {
-					// 	self.checkFiled($this);
-					// });
+					$this.on('keyup', function (event) {
+						self.checkFiled($this);
+					});
 				};
 			});
 			form.on('submit', function (event) {
@@ -54,15 +53,16 @@ class Validator {
 		if (!_.isNull(fieldRules)) {
 			let fieldRulesAry = fieldRules.split(';');
 			let isRequired = fieldRulesAry.indexOf('required') >= 0; // 是否必填
-			// 不必填不需要校验
-			if (!isRequired) {
+			// 不必填且空值不需要校验
+			if (!isRequired && fieldRules === '') {
 				return status;
 			};
 			// 其他规则
 			for (let index = 0, len = fieldRulesAry.length; index < len; index++) {
 				let currentRule = fieldRulesAry[index].trim();
-				// 必填
-				if (currentRule === '') continue;
+
+				if (currentRule === '') continue; // 规则为空
+
 				if (currentRule === 'required' && field.is(type[2])) {
 					// 单选、复选
 					if (self.form.find('[name="' + field.prop('name') + '"]:checked').length === 0) {
@@ -76,20 +76,26 @@ class Validator {
 						status = false;
 					};
 				} else {
+					// 其他
 					if (typeof this.rules[currentRule] === 'undefined') {
+						// 匹配不到规则
 						console.error('没有匹配到规则' + currentRule);
 					} else {
 						status = this.rules[currentRule].rule(fieldValue, field);
 					}
 				};
 				errorMsg = this.rules[currentRule] ? descriptions + ',' + this.rules[currentRule].msg : '空';
-				if (!status && self.settings.isFirstTime) {
-					self.showMsg(errorMsg);
-					field.focus();
+				self.borderColor(field, status);
+				if (!status) {
+					// 验证错误
 					self.settings.isFirstTime = false;
+					if (self.settings.isFirstTime) {
+						self.showMsg(errorMsg);
+						field.focus();
+					}
+					return status; // 退出当前field验证
 				}
 			}
-			return status;
 		}
 		return status;
 	}
@@ -100,6 +106,13 @@ class Validator {
 			return;
 		}
 		alert(msg);
+	}
+	borderColor (field, status) {
+		if (status) {
+			field.removeClass('color-error');
+		} else {
+			field.addClass('color-error');
+		};
 	}
 };
 export default Validator;
--- src/rules.js
+++ src/rules.js
@@ -11,7 +11,7 @@ let rules = {
 		msg: '请输入有效的电话号码',
 		rule: function (val) {
 			let reg = /^(0[0-9]{2,3}\-)?([2-9][0-9]{6,7})+(\-[0-9]{1,4})?$/;
-      return reg.test(val);
+			return reg.test(val);
 		}
 	},
 	// 手机号码
@@ -19,7 +19,39 @@ let rules = {
 		msg: '请输入有效的手机号码',
 		rule: function (val) {
 			let reg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
-      return reg.test(val);
+			return reg.test(val);
+		}
+	},
+	// 邮箱 email
+	email: {
+		msg: '请输入有效的邮箱',
+		rule: function (val) {
+			let reg = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)/;
+            return reg.test(val);
+		}
+	},
+	// 网址 url
+	url: {
+		msg: '请输入有效的网址',
+		rule: function (val) {
+			let reg = /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?/;
+			return reg.test(val);
+		}
+	},
+	// 邮政编码
+	postcode: {
+		msg: '请输入有效的邮编',
+		rule: function (val) {
+			let reg = /^[1-9][0-9]{5}$/;
+            return reg.test(val);
+		}
+	},
+	// 身份证号码
+	idcard: {
+		msg: '请输入有效的身份证号码',
+		rule: function (val) {
+			let reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+            return reg.test(val);
 		}
 	}
 };
